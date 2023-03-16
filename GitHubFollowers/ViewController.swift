ddcc84826734e64f65a849417ff64be1acd507a1
Dmytro Akulinin <akulinind@gmail.com>
2023-03-16 10:39:38 +0200

    Initial Commit

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Dmytro Akulinin on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
