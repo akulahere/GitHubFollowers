fbd357bdbab39db743332167f83dbb3d26fa2341
Dmytro Akulinin <akulinind@gmail.com>
2023-03-21 11:02:30 +0200

    fix: navbar title showing in transition between screens

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -14,6 +14,10 @@ class FollowerListViewController: UIViewController {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
     navigationController?.navigationBar.prefersLargeTitles = true
-    navigationController?.isNavigationBarHidden = false
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.setNavigationBarHidden(false, animated: true)
   }
 }
