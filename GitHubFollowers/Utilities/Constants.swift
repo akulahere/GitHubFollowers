0cfbeb0acdf86ef74dec07d571d6c2a5219caa80
Dmytro Akulinin <akulinind@gmail.com>
2023-04-04 15:03:12 +0300

    feat: implement custom Item Info View

--- GitHubFollowers/Utilities/Constants.swift
+++ GitHubFollowers/Utilities/Constants.swift
@@ -9,4 +9,9 @@ import Foundation
 
 enum SFSymbols {
   static let location = "mappin.and.ellipse"
+  static let repos = "folder"
+  static let gists = "text.alingnleft"
+  static let followers = "heart"
+  static let following = "person.2"
+  
 }
